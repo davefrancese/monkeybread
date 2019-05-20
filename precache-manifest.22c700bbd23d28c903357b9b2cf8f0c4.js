f2235fa99410da7bc95e42798ca5df9137309d10
dfrancese <davefrancese@gmail.com>
2019-05-20 17:58:10 -0400

    Updates

---
+++ precache-manifest.22c700bbd23d28c903357b9b2cf8f0c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30779d830910438ce213a51f12437b3",
+    "url": "/monkeybread/index.html"
+  },
+  {
+    "revision": "05abd4dc243c40c1d667",
+    "url": "/monkeybread/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/monkeybread/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "05abd4dc243c40c1d667",
+    "url": "/monkeybread/static/js/main.e04f70db.chunk.js"
+  },
+  {
+    "revision": "c577b6f76fb378e34eea",
+    "url": "/monkeybread/static/js/runtime~main.a4f319d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monkeybread/static/media/logo.5d5d9eef.svg"
+  }
+]);
